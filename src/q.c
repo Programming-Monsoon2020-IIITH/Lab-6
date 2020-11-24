a13b6897bd9ab7d0e8d22e2cccb176336348cb33
Nishanth Rachakonda <32260842+NishanthRachakonda@users.noreply.github.com>
2020-11-24 12:47:08 +0530

    updated source

--- src/q.c
+++ src/q.c
@@ -4,9 +4,10 @@
 #include "../utils/wrapper.h"
 
 int main(){
-    char read_file[1000] = "pp.jpg";
+    char read_file[1000] = "data/cat.jpg";
+    char write_file[1000] = "data/cat2.jpg";
     ImageIO *image_io = create_io_object();
     Image rgb_image = read_image(image_io, read_file);
-    write_image(image_io, read_file, rgb_image);
+    write_image(image_io, write_file, rgb_image);
     return 0;
 }
